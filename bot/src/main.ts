1306f6628d09399a2ac51142d26990c79a0ef2ce
Ray Cai Flanagan <hjkareus@gmail.com>
2022-12-19 16:04:27 -0500

    made

---
+++ bot/src/main.ts
@@ -0,0 +1,57 @@
+import { dirname, importx } from '@discordx/importer';
+import type { Interaction, Message } from 'discord.js';
+import { IntentsBitField } from 'discord.js';
+import { Client } from 'discordx';
+
+import { prisma } from 'bot-prisma';
+
+export const bot = new Client({
+  // Discord intents
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.GuildMessageReactions,
+    IntentsBitField.Flags.GuildVoiceStates,
+  ],
+
+  // Debug logs are disabled in silent mode
+  silent: false,
+
+  // Configuration for @SimpleCommand
+  simpleCommand: {
+    prefix: '!',
+  },
+});
+
+bot.once('ready', async () => {
+  // Synchronize applications commands with Discord
+  await bot.initApplicationCommands();
+
+  // To clear all guild commands, uncomment this line,
+  // This is useful when moving from guild commands to global commands
+  // It must only be executed once
+  //
+  //  await bot.clearApplicationCommands(
+  //    ...bot.guilds.cache.map((g) => g.id)
+  //  );
+
+  console.log('Bot started');
+});
+
+bot.on('interactionCreate', (interaction: Interaction) => {
+  bot.executeInteraction(interaction);
+});
+
+bot.on('messageCreate', (message: Message) => {
+  bot.executeCommand(message);
+});
+
+async function run() {
+  if (!process.env.BOT_TOKEN) throw Error('Could not find BOT_TOKEN in your environment');
+
+  await importx(`${ dirname(import.meta.url) }/{events,commands}/**/*.{ts,js}`);
+  await bot.login(process.env.BOT_TOKEN);
+}
+
+run();
